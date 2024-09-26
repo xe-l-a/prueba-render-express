0bf515f084c024996562d5a95b5c7328a24266b4
xe-l-a <168939083+xe-l-a@users.noreply.github.com>
2024-09-26 16:31:43 +0200

    crear ejs

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const app = express()
+const path = require('path')
 
 const port = process.env.PORT || 3001
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname,'views'))
+
+app.use(express.static(path.join(__dirname,'public')));
+
 app.get('/', (req, res) => {
-    res.send('Hola, mundo!');
+    res.render('index', { message: 'Prueba de Express y EJS'})
   });
   
-  app.listen(port, () => {
-    console.log(`Servidor escuchando en el puerto ${port}`);
-  });
+app.get('/mensaje', (req, res) => {
+res.send('Este es un mensaje enviado con un método GET.');
+});
+
+
+
+
+app.listen(port, () => {
+console.log(`Servidor escuchando en el puerto ${port}`);
+});
